788125bf0e530e3ce1f1f2a586a301eba6c46252
mafyyu <morita0126m@gmail.com>
2025-11-08 17:31:46 +0900

    API修正

--- src/app/api/posts/[id]/like/route.ts
+++ src/app/api/posts/[id]/like/route.ts
@@ -1,29 +1,84 @@
 import { supabase } from "@/libs/supabaseClient";
 import { NextRequest, NextResponse } from "next/server";
+// import { currentUser } from "@clerk/nextjs/server";
+
+// clerk導入後にuser_idをuser.idに変更してsupabaseのuser_idをuuidに変更
+
 
 export async function POST(
     _request: NextRequest,
     context: { params: Promise<{ id: string }> }){
     try{
-        const { id } = await context.params
+        const userId = "test_user"
+        const { id } = await context.params;
+        const postId = Number(id)
 
-        const { data: postData, error: fetchError } = await supabase
-            .from("posts")
-            .select("like")
-            .eq("id", Number(id))
-            .single();
-        if(fetchError) throw new Error(fetchError.message)
+        // const user = await currentUser();
+        // if(!user){
+        //     return NextResponse.json({ error: "Unauthorized"}, { status: 401 })
+        // }
+
+        const { data: existingLike, error: likeFetchError } = await supabase
+            .from("like")
+            .select("*")
+            .eq("post_id", postId)
+            .eq("user_id", userId)
+        if(likeFetchError) throw new Error(likeFetchError.message)
+
+        //　既存のいいね数を取得
+    const { data: postData, error: fetchError } = await supabase
+        .from("posts")
+        .select("like")
+        .eq("id", Number(postId))
+        .single();
+    if(fetchError) throw new Error(fetchError.message)
+    
+    const currentLike: number = postData.like ?? 0;
         
-        const currentLike: number = postData.like;
+        // 既にいいねをしていたら取り消し処理
+        if(existingLike && existingLike.length > 0){
+            const { error: deleteError } = await supabase
+                .from('like')
+                .delete()
+                .eq("user_id", userId)
+                .eq("post_id", postId)
+            if(deleteError) throw new Error(deleteError.message);
 
-        const { data: updateData, error: updateError } = await supabase
+            // いいね数を減らす
+            const { error: updateError } = await supabase
+                .from("posts")
+                .update({like: currentLike - 1})
+                .eq("id", postId)
+                .select()
+            if(updateError) throw new Error(updateError.message);
+            return NextResponse.json(
+                { message: "success delete", newLikeCount: currentLike - 1 },
+                { status: 200 }
+            )
+        }
+
+        // 新規like追加
+        const { error: insertError } = await supabase
+            .from("like")
+            .insert([{
+                post_id: postId,
+                user_id: userId
+            }])
+        if(insertError) throw new Error(insertError.message)
+
+
+        // いいね数を1増やす
+        const { error: updateError } = await supabase
             .from("posts")
             .update({like: currentLike + 1})
-            .eq("id",id)
+            .eq("id", postId)
             .select()
         if(updateError) throw new Error(updateError.message);
 
-        return NextResponse.json(updateData, { status:200 });
+        return NextResponse.json(
+            { message: "Liked", newLikeCount: currentLike + 1 },
+            { status:200 }
+        );
 
     }catch(error){
         console.error(error);
--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,12 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import { supabase } from "@/libs/supabaseClient";
+// import { currentUser } from "@clerk/nextjs/server";
 
 export async function POST(request: NextRequest) {
     try{
-        const { user_id, content } = await request.json();
+        const userId = "test_user"
+        // const user = await currentUser();
+        // if (!user) {
+        //     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        //   }
+        const { content } = await request.json();
+        if(content.trim()=="" || !content){
+            return NextResponse.json(
+                { error: "Content cannnot be empty" },
+                { status: 400 }
+            )
+        }
         const { data, error } = await supabase
             .from('posts')
-            .insert([{ user_id, content }])
+            .insert([{ user_id: userId, content }])
             .select()
 
         if (error) {
@@ -29,24 +41,42 @@ export async function GET(request: NextRequest){
     try{
         const { searchParams }  = new URL(request.url);
         const limit = Number(searchParams.get("limit") || 10)
-        const cursor = searchParams.get("cursor");
+        const cursorCreatedAt = searchParams.get("cursor_created_at");
+        const cursorId = searchParams.get("cursor_id");
+
 
         let query = supabase
             .from('posts')
-            .select("*")
+            .select("*", { count: "exact" })
             .order("created_at",{ ascending: false })
             .order("id", { ascending: false })
-            .limit(limit)
+            .limit(limit+1)
 
-        if(cursor && cursor.trim() !="" ) {
-            query = query.lt('created_at',cursor)
+        if(cursorId && cursorCreatedAt) {
+            query = query.or(
+                `and(created_at.lt.${cursorCreatedAt}),and(created_at.eq.${cursorCreatedAt},id.lt.${cursorId})`
+            );
         }
 
         const { data, error } = await query;
-
         if (error) throw new Error(error.message);
+        // まだ投稿があるか
+        const hasMore = data.length >= limit;
+        // 10件のみ返す
+        const posts = hasMore ? data?.slice(0, limit) :data;
 
-        return NextResponse.json(data, { status:200 })
+        const nextCursor = 
+            posts.length > 0 
+                ? {
+                    cursor_created_at: posts.at(-1).created_at,
+                    cursor_id: posts.at(-1).id
+                }
+            :null
+        console.log("hasMore:", hasMore);
+        return NextResponse.json(
+            { posts, nextCursor, hasMore },
+            { status:200 }
+        )
 
     } catch(error){
         console.error(error)
